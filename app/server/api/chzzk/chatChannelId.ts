aac3a341fef5125af9d799ec963359769e44cf3d
Nesswit <rishubil@gmail.com>
2024-03-01 15:11:11 +0000

    Suppress server errors to prevent credential leakage

--- app/server/api/chzzk/chatChannelId.ts
+++ app/server/api/chzzk/chatChannelId.ts
@@ -2,31 +2,38 @@ import { ChzzkChatChannelIdResponse, ApiError } from "~/lib/interfaces";
 
 export default defineEventHandler(
   async (event): Promise<ChzzkChatChannelIdResponse | ApiError> => {
-    const query = getQuery(event);
-    const channelId = query.channelId;
-    if (!channelId || typeof channelId !== "string") {
+    try {
+      const query = getQuery(event);
+      const channelId = query.channelId;
+      if (!channelId || typeof channelId !== "string") {
+        return {
+          status: "ERROR",
+          error: "channelId param should be a string",
+        };
+      }
+      const response = await $fetch<{
+        content?: {
+          chatChannelId?: string;
+        };
+      }>(
+        `https://api.chzzk.naver.com/polling/v2/channels/${channelId}/live-status`
+      );
+      if (response?.content?.chatChannelId === undefined) {
+        return {
+          status: "ERROR",
+          error: "No chatChannelId in response",
+        };
+      }
+      const chatChannelId = response.content.chatChannelId;
+      return {
+        status: "OK",
+        chatChannelId: chatChannelId,
+      };
+    } catch (error) {
       return {
         status: "ERROR",
-        error: "channelId param should be a string",
+        error: "Internal Server Error",
       };
     }
-    const response = await $fetch<{
-      content?: {
-        chatChannelId?: string;
-      };
-    }>(
-      `https://api.chzzk.naver.com/polling/v2/channels/${channelId}/live-status`
-    );
-    if (response?.content?.chatChannelId === undefined) {
-      return {
-        status: "ERROR",
-        error: "No chatChannelId in response",
-      };
-    }
-    const chatChannelId = response.content.chatChannelId;
-    return {
-      status: "OK",
-      chatChannelId: chatChannelId,
-    };
   }
 );
--- app/server/api/youtubeLive/proxy/[...path].ts
+++ app/server/api/youtubeLive/proxy/[...path].ts
@@ -3,26 +3,33 @@ import { ApiError } from "~/lib/interfaces";
 
 export default defineEventHandler(
   async (event): Promise<unknown | ApiError> => {
-    if (!event.node.req.url) {
+    try {
+      if (!event.node.req.url) {
+        return {
+          status: "ERROR",
+          error: "No url in request",
+        };
+      }
+      if (!event.context.params?.path) {
+        return {
+          status: "ERROR",
+          error: "No path in params",
+        };
+      }
+      const url = new URL(event.node.req.url, "https://www.youtube.com");
+      url.pathname = event.context.params.path;
+      return proxyRequest(event, url.toString(), {
+        headers: {
+          host: url.host,
+          origin: url.origin,
+          referer: url.origin,
+        },
+      });
+    } catch (error) {
       return {
         status: "ERROR",
-        error: "No url in request",
+        error: "Internal Server Error",
       };
     }
-    if (!event.context.params?.path) {
-      return {
-        status: "ERROR",
-        error: "No path in params",
-      };
-    }
-    const url = new URL(event.node.req.url, "https://www.youtube.com");
-    url.pathname = event.context.params.path;
-    return proxyRequest(event, url.toString(), {
-      headers: {
-        host: url.host,
-        origin: url.origin,
-        referer: url.origin,
-      },
-    });
   }
 );
