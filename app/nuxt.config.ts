ed1b30216074c30757c134c7a7036b34d4801c2d
Nesswit <rishubil@gmail.com>
2024-03-10 07:00:15 +0000

    Add GA

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ["@pinia/nuxt"],
+  modules: ["@pinia/nuxt", "nuxt-gtag"],
   imports: {
     dirs: ["composables/**"],
   },
